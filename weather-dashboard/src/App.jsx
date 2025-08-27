091138f05f6eba5155ceb351426ded5865a428e1
arefatmoh <arefat.mohammed-ug@aau.edu.et>
2025-08-27 15:03:16 +0300

    Add loading state while fetching weather and Make it Mobile responsiveness

--- weather-dashboard/src/App.jsx
+++ weather-dashboard/src/App.jsx
@@ -6,10 +6,14 @@ import { fetchWeather } from "./services/weatherService";
 function App() {
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = async (city) => {
     setError("");
+    setLoading(true);
     const data = await fetchWeather(city);
+    setLoading(false);
+
     if (data) {
       setWeather(data);
     } else {
@@ -23,8 +27,9 @@ function App() {
       <h1 className="text-4xl font-bold text-white mb-6">🌤️ Weather Dashboard</h1>
       <SearchBar onSearch={handleSearch} />
 
+      {loading && <p className="text-white mt-4 animate-pulse">Loading...</p>}
       {error && <p className="text-red-200 mt-4">{error}</p>}
-      {weather && <WeatherCard data={weather} />}
+      {weather && !loading && <WeatherCard data={weather} />}
     </div>
   );
 }
