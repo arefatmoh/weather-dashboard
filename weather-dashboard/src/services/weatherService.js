434f94e50aed73d95747fad05bd0ef51597b2c8d
arefatmoh <arefat.mohammed-ug@aau.edu.et>
2025-08-27 15:11:47 +0300

    Create ForecastCard component

--- weather-dashboard/src/services/weatherService.js
+++ weather-dashboard/src/services/weatherService.js
@@ -11,3 +11,17 @@ export async function fetchWeather(city) {
     return null;
   }
 }
+
+export async function fetchForecast(city) {
+  try {
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`
+    );
+    if (!res.ok) throw new Error("Forecast not available");
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
